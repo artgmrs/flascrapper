da405344342a0a864295b779799ead035ab82929
Arthur Guimarães <39536084+artgmrs@users.noreply.github.com>
2024-02-08 13:32:53 -0300

    Cors

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,33 @@
 // import type { VercelRequest, VercelResponse } from '@vercel/node'
 const { getNextGameAsync } = require('../src/services/scraperService.js');
 const NodeCache = require("node-cache");
+// import type { NextApiRequest, NextApiResponse } from "next";
+const { Cors } = require("cors");
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+  origin: "*",
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req,
+  res,
+  fn
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const myCache = new NodeCache({
   stdTTL: 86400,
@@ -11,6 +38,9 @@ export default async function handler(req, res) {
   // return res.json({
   //   message: `Hello ${name}!`,
   // })
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   try {
     console.log('chegou')
     const key = req.url;
